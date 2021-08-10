9f1e963c96a1443f8bf93cc154a7a0d05975d385
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-10 20:12:55 +0530

    Add: Method to let Player choose letter to play game implemented

--- src/com/company/TicTacToeGame.java
+++ src/com/company/TicTacToeGame.java
@@ -1,17 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * Tic Tac Toe Game designed: Board made using char array
  */
 public class TicTacToeGame {
     //variables
     static char[] board=new char[10];
-
+    static char playerLetter,computerLetter;
     // method to assign empty spaces to board
     static void boardCreate(){
         // 0th index ignored
         for(int i=1;i<board.length;i++) {
             board[i] = ' ';
+        }
+    }
+    // method checks player letter choice and sets
+    static void playerChoice(){
+        System.out.println("Choose letter X or O to play game");
+        Scanner sc=new Scanner(System.in);
+        playerLetter=sc.next().toUpperCase().charAt(0);
+        switch(playerLetter){
+            case 'X':
+                System.out.println("Player Plays: X ");
+                System.out.println("Computer Plays: O ");
+                computerLetter='O';
+                break;
+            case 'O':
+                System.out.println("Player Plays: O ");
+                System.out.println("Computer Plays: X");
+                computerLetter='X';
+                break;
+            default:
+                System.out.println("Incorrect Input");
+                break;
         }
     }
 
@@ -20,5 +43,6 @@ public class TicTacToeGame {
         System.out.println("Welcome to Tic Tac Toe Game");
         // game board created
         boardCreate();
+        playerChoice();
     }
 }
